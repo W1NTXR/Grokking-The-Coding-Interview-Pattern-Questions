fc64c0374162b1ea9eda26e662b39efd26d31381
W1NTXR <92108800+W1NTXR@users.noreply.github.com>
2022-11-20 22:33:54 +0530

    D11

--- 3.Pattern - Fast And Slow Pointers/Linked List Cycle.cpp
+++ 3.Pattern - Fast And Slow Pointers/Linked List Cycle.cpp
@@ -1,45 +1,96 @@
+/*
+Leetcode link: https://leetcode.com/problems/linked-list-cycle/
+Problem Description: 
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+*/
 #include<iostream>
 using namespace std;
 
-struct node {
+struct ListNode {
     int data;
-    node* next;
-
-    node(int x) : data(x), next(NULL){}
-    
-
-    
+    ListNode* next;
+    ListNode(int x): data(x), next(NULL){}
 };
-void pb(int x, node *head) {
-        if(head==NULL) {new node(x); return;}
-        node* h = new node(x);
-        node* t = h;
-        while(t != NULL) {
-            t = head->next;
+
+struct LL{
+    ListNode* head;
+    public:
+    LL(): head(NULL) {}
+    void pb(int data) {
+        ListNode* nnode = new ListNode(data);
+        if(head == NULL) {
+            head = nnode;
+            return;
         }
-        t -> next = h;
+
+        ListNode* t = head;
+        while(t->next != NULL) {
+            t = t->next;
+        }
+
+        t->next = nnode;    
     }
-void disp(node *head) {
-        node* t = head;
+
+    void disp() {
+        ListNode* t = head;
         while(t != NULL) {
-            cout << t << " ";
+            cout << t->data << " ";
             t = t->next;
         }
     }
+};
 
-bool hasCycle(node *head){
-    return false;
+bool hasCycle(ListNode *head){
+if(head == NULL) return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL) {
+            
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow == fast) return true;
+        }
+
+        return false;
 }
 
 int main(){
     int n;
-    node *x;
+    LL x;
     cin >> n;
     for(int i = 0; i < n; i ++) {
         int l;
         cin >> l;
-        pb(l, x);
+        x.pb(l);
     }
 
-    disp(x);
+    return 0;
 }
