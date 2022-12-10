eb85dbab9ac3cc88016d80bfcaaf3904daa3cab0
W1NTXR <92108800+W1NTXR@users.noreply.github.com>
2022-12-10 16:38:49 +0530

    D17

---
+++ 4.Pattern - Merge Intervals/Employee Free Time.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> EmployeeFreeTime(vector<vector<int>> schedule){
+    vector<vector<int>> ans;
+    sort(schedule.begin(),schedule.end());
+    int n = schedule.size();
+    int i=0,max=schedule[0].back(),min;
+    while(i<n) {
+        if(max < schedule[i].back()) {
+            if(max<schedule[i].front()) return {{max,schedule[i].front()}};
+            max=schedule[i].back();
+        }
+        i++;
+    }
+    return {{-1,-1}};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> x(n,vector<int>(2,0));
+    for(int i = 0; i < n; i++) cin >> x[i][0] >> x[i][1];
+    for(auto i: EmployeeFreeTime(x)) cout << i[0] << " " << i[1];
+}
